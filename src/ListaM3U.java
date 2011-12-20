b86d6e19f6e83762a35dbd0f4845fb9d6b94159e
szymon.sobocik <szymon.sobocik@qnt.pl>
2011-12-20 14:24:18 +0100

    Lista jest tworzona tylko w katalogach, które zawierają obsługiwane typy plików.

--- src/ListaM3U.java
+++ src/ListaM3U.java
@@ -23,29 +23,45 @@ public class ListaM3U {
 
     public static void utworzListeM3U(File katalog, boolean recursive) {
         if (katalog.isDirectory()) {
-            try {
-                FileWriter fileWriter = new FileWriter(katalog.getCanonicalPath() + "\\" + katalog.getName() + ".m3u");
-                PrintWriter printWriter = new PrintWriter(fileWriter);
+            if (containsSupportedFiles(katalog)) {
+                try {
+                    FileWriter fileWriter = new FileWriter(katalog.getCanonicalPath() + "\\" + katalog.getName() + ".m3u");
+                    PrintWriter printWriter = new PrintWriter(fileWriter);
 
-                for (File plik : katalog.listFiles()) {
-                    if (plik.isFile()) {
-                        for (String typPliku : plikiObslugiwane) {
-                            if (plik.getName().endsWith(typPliku)) {
-                                printWriter.println(plik.getName());
+                    for (File plik : katalog.listFiles()) {
+                        if (plik.isFile()) {
+                            for (String typPliku : plikiObslugiwane) {
+                                if (plik.getName().endsWith(typPliku)) {
+                                    printWriter.println(plik.getName());
+                                }
                             }
                         }
                     }
+                    printWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                 }
-                printWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
-
             if (recursive) {
                 for (File podkatalog : katalog.listFiles()) {
                     utworzListeM3U(podkatalog, recursive);
                 }
             }
         }
+    }
+
+    private static boolean containsSupportedFiles(File katalog) {
+        if (katalog.isDirectory()) {
+            for (File file : katalog.listFiles()) {
+                if (file.isFile()) {
+                    for (String typPliku : plikiObslugiwane) {
+                        if (file.getName().endsWith(typPliku)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
