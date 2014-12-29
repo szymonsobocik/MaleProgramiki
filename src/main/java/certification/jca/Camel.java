bbd040597c7884a051028561542d2710c5bae559
Szymon Sobocik <ssobocik@future-processing.com>
2014-12-29 09:17:04 +0100

    Certification.jca

---
+++ src/main/java/certification/jca/Camel.java
@@ -0,0 +1,21 @@
+package certification.jca;
+
+interface Rideable {
+    String getGait();
+}
+public class Camel implements Rideable {
+    int weight = 2;
+    public static void main(String[] args) {
+        new Camel().go(8);
+    }
+    void go(int speed) {
+        ++speed;
+        weight++;
+        int walkrate = speed * weight;
+        System.out.print(walkrate + getGait());
+    }
+    public String getGait() {
+//    String getGait() {
+        return " mph, lope";
+    }
+}
---
+++ src/main/java/certification/jca/Cougar.java
@@ -0,0 +1,24 @@
+package certification.jca;
+
+class Feline {
+    public String type = "f ";
+
+    public Feline() {
+        System.out.print("feline ");
+    }
+}
+
+public class Cougar extends Feline {
+    public Cougar() {
+        System.out.print("cougar ");
+    }
+
+    public static void main(String[] args) {
+        new Cougar().go();
+    }
+
+    void go() {
+        type = "c ";
+        System.out.print(this.type + super.type);
+    }
+}
